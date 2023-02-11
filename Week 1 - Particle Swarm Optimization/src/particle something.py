e71c0cd32bc06f97e3f2e188bbcb6cf6902a6e51
sauravzach <50353051+sauravzach@users.noreply.github.com>
2023-02-11 22:37:08 +0100

    Add files via upload

--- Week 1 - Particle Swarm Optimization/src/particle something.py
+++ Week 1 - Particle Swarm Optimization/src/particle something.py
@@ -1,10 +1,12 @@
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 from matplotlib.colors import LightSource
 
-N = 100 #no of particles
-np.random.seed(42)
+N = 200
+np.random.seed(420)
 def benchmark_rosenbrock(x, y, a=1, b=100):
     # Min at (x=a, y=a**2)
     return (a - x) ** 2 + b * (y - x**2) ** 2
@@ -19,17 +21,17 @@ def benchmark_rastrigin(x, y, n=2, A=10):
     # Min at (x=0, y=0)
     #return _benchmark_rastrigin(x, n, A) + _benchmark_rastrigin(y, n, A)
 
-min_x = -5.12
-max_x = 5.12
-min_y = -5.12
-max_y = 5.12
-benchmark = benchmark_rastrigin
+# min_x = -5.12
+# max_x = 5.12
+# min_y = -5.12
+# max_y = 5.12
+# benchmark = benchmark_rastrigin
 #
-# min_x = -1.8
-# max_x = 1.8
-# min_y = -1
-# max_y = 3
-# benchmark = benchmark_rosenbrock
+min_x = -1.8
+max_x = 1.8
+min_y = -1
+max_y = 3
+benchmark = benchmark_rosenbrock
 
 # weights for velocity
 
@@ -84,42 +86,46 @@ z = benchmark(x, y)
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-ax.plot_surface(x, y, z, cmap='ocean',antialiased=True,edgecolor='none',alpha=0.6)
+ax.plot_surface(x, y, z, cmap='hsv',antialiased=True,edgecolor='none',alpha=0.6)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
-# plt.show()
 
 particle_history = [swarm_pos]
 scatter = ax.scatter(
     swarm_pos[:, 0],
     swarm_pos[:, 1],
     [benchmark(x, y) for x, y in swarm_pos],
-    c=(10,20,1,2,21,3,13,21,321,213,21,321,31,32,132,13,21,321,32,1),
+    c=[benchmark(x, y) for x, y in swarm_pos],
     linewidths=1,
-    label="scatter",
+    label="scatter",alpha=1
 )
 epoch = 1
-while veloc.max() > 1e-2:
+# while ((round(abs(np.mean(swarm_pos[:,0])),2)>0 and round(abs(np.mean(swarm_pos[:,1])),2)>0)):
+vel=np.array([math.sqrt(x**2+y**2) for x,y in veloc])
+
+while np.mean(vel) > 1e-2:
 # for i in range(20):  #epoch count
    # for i in range(swarm_count): # for each particle update velocity
-   ax.set_title(f"Particle Swarm Optimization - epoch:{epoch}")
-   R = np.random.uniform(0, 1)
-   veloc = aa*veloc+(bb*R*(score_pos[:,1:]-swarm_pos))+(cc*R*(global_pos[1:]-swarm_pos))
-   swarm_pos=swarm_pos+veloc
-   new_scores=np.array([benchmark(*p) for p in swarm_pos])
-   for i in range(swarm_count):
+    ax.set_title(f"Particle Swarm Optimization - epoch:{epoch}")
+    R = np.random.uniform(0, 1)
+    veloc = aa*veloc+(bb*R*(score_pos[:,1:]-swarm_pos))+(cc*R*(global_pos[1:]-swarm_pos))
+    vel=np.array([math.sqrt(x**2+y**2) for x,y in veloc])
+    swarm_pos=swarm_pos+veloc
+    new_scores=np.array([benchmark(*p) for p in swarm_pos])
+    for i in range(swarm_count):
         if(new_scores[i]<score_pos[i][0]):
             if(new_scores[i]<global_pos[0]):
                 global_pos = [new_scores[i],swarm_pos[i][0],swarm_pos[i][1]]
             score_pos[i]=[new_scores[i],swarm_pos[i][0],swarm_pos[i][1]]
-   particle_history.append(swarm_pos)
-   scatter.set_offsets([[_x, _y] for _x, _y in swarm_pos])
-   scatter.set_3d_properties([benchmark(x, y) for x, y in swarm_pos], "z")
-   epoch+=1
-   aa=aa/1.01
-   plt.draw()
-   plt.pause(0.3)
+    particle_history.append(swarm_pos)
+    scatter.set_offsets([[_x, _y] for _x, _y in swarm_pos])
+    scatter.set_3d_properties([benchmark(x, y) for x, y in swarm_pos], "z")
+    epoch+=1
+    aa=aa/1.01
+    plt.draw()
+    plt.pause(0.1)
+    #print(np.mean(vel))
 plt.show()
 print(global_pos)
 print(f"score and pos:{score_pos}")
