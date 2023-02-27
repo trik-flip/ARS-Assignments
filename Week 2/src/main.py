b71310b4cd9ed754431249af887527304a707a3c
Philip Bollen <flip.philip@hotmail.com>
2023-02-27 23:14:10 +0100

    discrete controls

--- Week 2/src/main.py
+++ Week 2/src/main.py
@@ -14,7 +14,7 @@ BLACK = (0, 0, 0)
 
 
 (width, height) = (1900, 1060)
-STEP_SIZE = 0.001
+STEP_SIZE = 0.1
 
 
 box_x1 = 200
@@ -22,7 +22,6 @@ box_x2 = 1700
 box_y1 = 200
 box_y2 = 880
 
-box_tuple = (box_x1, box_y1, box_x2, box_y2)
 background = WHITE
 running = True
 pygame.init()
@@ -41,6 +40,15 @@ line = Line(box_x1, box_y1, box_x2 / 2, box_y2 / 2)
 line2 = Line(box_x2 * 3 / 4, box_y2, box_x2, box_y1)
 
 map = [line2, line] + box.lines()
+
+pushed = [False in range(6)]
+o_pushed = False
+l_pushed = False
+w_pushed = False
+s_pushed = False
+t_pushed = False
+g_pushed = False
+
 while running:
     screen.fill(background)
     ev = pygame.event.get()
@@ -53,23 +61,36 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     key_event = pygame.key.get_pressed()
-    if key_event[pygame.K_o]:
+
+    if key_event[pygame.K_o] and o_pushed == False:
         robby.speed.right += STEP_SIZE
-    if key_event[pygame.K_l]:
+    o_pushed = key_event[pygame.K_o]
+    
+    if key_event[pygame.K_l] and l_pushed == False:
         robby.speed.right -= STEP_SIZE
-    if key_event[pygame.K_w]:
+
+    l_pushed = key_event[pygame.K_l]
+    if key_event[pygame.K_w] and w_pushed == False:
         robby.speed.left += STEP_SIZE
-    if key_event[pygame.K_s]:
+
+    w_pushed = key_event[pygame.K_w]
+    if key_event[pygame.K_s] and s_pushed == False:
         robby.speed.left -= STEP_SIZE
-    if key_event[pygame.K_t]:
+
+    s_pushed = key_event[pygame.K_s]
+    if key_event[pygame.K_t] and t_pushed == False:
         robby.speed.left += STEP_SIZE
         robby.speed.right += STEP_SIZE
-    if key_event[pygame.K_g]:
+
+    t_pushed = key_event[pygame.K_t]
+    if key_event[pygame.K_g] and g_pushed == False:
         robby.speed.left -= STEP_SIZE
         robby.speed.right -= STEP_SIZE
+
+    g_pushed = key_event[pygame.K_g]
     if key_event[pygame.K_r]:
-        robby.position.x=1920 / 2
-        robby.position.y=1080 / 2
+        robby.position.x = 1920 / 2
+        robby.position.y = 1080 / 2
         robby.speed.left = 0
         robby.speed.right = 0
         robby.direction = pi
