07cf2819b612ece0433e64cdd70a9ef25d75abf5
Samyak-Sheth <shethsamy@outlook.com>
2023-03-06 14:36:26 +0100

    Added plot

--- week3_evolutionary_algorithm/__main__.py
+++ week3_evolutionary_algorithm/__main__.py
@@ -1,23 +1,79 @@
 import numpy as np
+from src.benchmarks import rastrigin, rosenbrock
+from src.simple.evolutionary_algorithm import EvolutionaryAlgorithm
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import LightSource
 
-from .src.benchmarks import rastrigin, rosenbrock
-from .src.simple.evolutionary_algorithm import EvolutionaryAlgorithm
+
+benchmark = rosenbrock
+
+def visual(benchmark): 
+    print()
 
 ea = EvolutionaryAlgorithm(population_size=20, input=0, output=2)
+x_and_ys = [p.run() for p in ea.population]
+
+#region benchmark plot
+min_x, max_x = -1.8, 1.8
+min_y, max_y = -1, 3
+x = np.linspace(min_x, max_x, 300)
+y = np.linspace(min_y, max_y, 300)
+x, y = np.meshgrid(x, y)
+z = benchmark(x, y)
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1, projection="3d")
+ls = LightSource(500, 90)
+surf = ax1.plot_surface(
+    x, y, z, cmap=cm.jet, edgecolor="black", linewidth=0.02, zorder=-1, alpha=0.1
+)
+#endregion
+
+#region plot particles based on the EA initialisation
+particles_x=[coord[0] for coord in x_and_ys]
+particles_y=[coord[1] for coord in x_and_ys]
+particles = np.array([ele for ele in x_and_ys])
+
+scatter = ax1.scatter(
+    particles_x,
+    particles_y,
+    [benchmark(x, y) for x, y in particles],
+    c=[0 for _ in particles],
+    linewidths=0,
+    label="scatter",
+)
+#endregion
+
+
+plt.draw()
+plt.waitforbuttonpress()
+
+
 best = float("inf")
 unchanged = 0
 epoch = 0
+
+
+benchmark = rosenbrock
 while unchanged < 100:
     epoch += 1
-    ea.epoch((), rosenbrock)
+    ea.epoch((), benchmark)
     ea.selection()
     ea.repopulation()
 
     (org,) = ea.best()
     result = org.run()
-    print(epoch, result, rosenbrock(*result))
-    if best > rosenbrock(*result):
+
+    x_and_ys = [p.run() for p in ea.population]
+    scatter.set_offsets([ele for ele in x_and_ys])
+    scatter.set_3d_properties([benchmark(x, y) for x, y in x_and_ys], "z")
+    plt.draw()
+    plt.pause(0.1)
+
+    print(epoch, result, benchmark(*result))
+    if best > benchmark(*result):
         unchanged = 0
-        best = rosenbrock(*result)
+        best = benchmark(*result)
     else:
         unchanged += 1
+plt.show()
